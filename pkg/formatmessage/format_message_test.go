f93cb5c282c0e1e55b9a1227f9598a814d3323e6
Cecile Yang <cecileyng@gmail.com>
2021-11-11 15:17:16 +0000

    add TestGetAssigneeHasMultipleRequestedReviewersrecording

--- pkg/formatmessage/format_message_test.go
+++ pkg/formatmessage/format_message_test.go
@@ -1,12 +1,16 @@
 package formatmessage
 
-import "testing"
+import (
+	"testing"
+	"github.com/dnaeon/go-vcr/v2/recorder"
+)
+	
 
 func TestGetAssigneeEmptyRequestedReviewers(t *testing.T) {
 	body := Webhook{}
 	have := GetAssignee(body)
 	want := ""
-	if have != want {
+	if have != want  {
 		t.Fatalf("Have: %s, want: %s", have, want)
 	}
 }
@@ -29,4 +33,39 @@ func TestGetAssigneeHasRequestedReviewers(t *testing.T) {
 	if have != want {
 		t.Fatalf("Have: %s, want: %s", have, want)
 	}
+
+func TestGetAssigneeHasMultipleRequestedReviewers(t *testing.T) {
+	want := "someone"
+	reviewers := []RequestedReviewer{
+		{
+			Login: "not me",
+		},
+		{
+			Login: want,
+		},
+	}
+
+	example_response.json
+
+	body := Webhook{
+		PullRequest: PullRequest{
+			RequestedReviewers: reviewers,
+		},
+	}
+
+	func getBodyFromReq() {
+		fileWithReq := openRecording()
+
+		body := fileWithReq.body
+
+		return body
+	}
+
+	body := getBodyFromReq()
+
+	have := GetAssignee(body)
+	if have != want {
+		t.Fatalf("Have: %s, want: %s", have, want)
+	}
+	recorder
 }
