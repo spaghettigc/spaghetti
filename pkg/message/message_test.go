235e032f12cefd4d10dbe0653e649ab172c66f88
Cecile Yang <cyang@gocardless.com>
2022-05-12 11:41:14 +0100

    add test for formatmessage success

--- pkg/message/message_test.go
+++ pkg/message/message_test.go
@@ -7,14 +7,40 @@ import (
 	. "github.com/onsi/gomega"
 )
 
-func Test_TruncateShortMessage(t *testing.T) {
+func Test_Truncate_ShortMessage(t *testing.T) {
 	g := NewGomegaWithT(t)
 	text := "less than 100 chars"
 	g.Expect(message.Truncate(text)).To(Equal(text))
 }
 
-func Test_TruncateLongMessage(t *testing.T) {
+func Test_Truncate_LongMessage(t *testing.T) {
 	g := NewGomegaWithT(t)
 	text := "long long long long long long long long long long long long long long long long long long long long long test: more than 100 chars"
 	g.Expect(message.Truncate(text)).To(Equal(text[:100] + "..."))
+}
+
+func Test_FormatMessage_UserMappingSuccess(t *testing.T) {
+	g := NewGomegaWithT(t)
+	url := "https://github.com/dontgohere/pulls/9999/"
+	title := "fix: wow we componentised pricing"
+	body := "we did it"
+	assignee := message.Assigned{
+		User: "githubUser",
+		Team: "Billing and revenue",
+	}
+	requester := "requester_user"
+	userMapping := message.UserMap{
+		"githubUser": "slackUser",
+	}
+
+	opt := message.FormatMessageOptions{
+		URL:         url,
+		Title:       title,
+		Body:        body,
+		Assignee:    assignee,
+		Requester:   requester,
+		UserMapping: userMapping,
+	}
+	message := message.FormatMessage(opt)
+	g.Expect(message).To(Equal("<@slackUser> was assigned. PR title: fix: wow we componentised pricing (https://github.com/dontgohere/pulls/9999/).\nBilling and revenue team was requested to review by requester_user. \nwe did it \n"))
 }
